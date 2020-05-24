a64976ef699f750a4d9f99c9a8a4757121aeaa27
Murray Heymann <heymann.murray@gmail.com>
2020-05-24 17:37:40 +0200

    Update numbers

--- draw_graphs.sh
+++ draw_graphs.sh
@@ -5,6 +5,7 @@
 ./graph_stats.py dataset=tests yscale=linear graphtype=daily
 ./graph_stats.py dataset=deaths
 ./graph_stats.py dataset=deaths yscale=linear
+./graph_stats.py dataset=deaths yscale=linear graphtype=daily
 ./graph_stats.py dataset=recov yscale=linear graphtype=daily n_day_av=3
 ./graph_stats.py dataset=postests yscale=linear
 ./graph_stats.py dataset=postests yscale=linear graphtype=daily n_day_av=3
