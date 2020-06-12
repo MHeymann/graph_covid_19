402040e479201a4165e95eaf72704f39d1a8d1c8
Murray Heymann <heymann.murray@gmail.com>
2020-06-12 16:20:07 +0200

    Update data

--- dates.sh
+++ dates.sh
@@ -3,7 +3,10 @@
 startdate=2020-03-15
 enddate=$( date +%Y-%m-%d )
 
-curr=$( date +%Y-%m-%d --date "$startdate -1 day" )
+#curr=$( date +%Y-%m-%d --date "$startdate -1 day" )
+curr=$( date +%Y-%m-%d --date "$( date +%Y-%m-%d ) -1 day" )
+curr=$( date +%Y-%m-%d --date "$curr -1 day" )
+curr=$( date +%Y-%m-%d --date "$curr -1 day" )
 while true; do
     curr=$( date +%Y-%m-%d --date "$curr +1 day" )
     echo "$curr"
