47631ebad78991a42835c2500b11199b1fca63da
Murray Heymann <heymann.murray@gmail.com>
2021-01-29 09:17:36 +0100

    Update SA data up to 21 Dec

--- draw_graphs.sh
+++ draw_graphs.sh
@@ -2,15 +2,23 @@
 
 ./graph_stats.py dataset=pos $1
 ./graph_stats.py dataset=pos yscale=linear graphtype=daily $1
+./graph_stats.py dataset=pos yscale=linear graphtype=daily n_day_av=7 $1
+
 ./graph_stats.py dataset=tests $1
 ./graph_stats.py dataset=tests yscale=linear graphtype=daily $1
+./graph_stats.py dataset=tests yscale=linear graphtype=daily n_day_av=7 $1
+
 ./graph_stats.py dataset=deaths $1
 ./graph_stats.py dataset=deaths yscale=linear $1
 ./graph_stats.py dataset=deaths yscale=linear graphtype=daily $1
-./graph_stats.py dataset=recov yscale=linear graphtype=daily n_day_av=3 $1
+./graph_stats.py dataset=deaths yscale=linear graphtype=daily n_day_av=7  $1
+
+./graph_stats.py dataset=recov yscale=linear graphtype=daily n_day_av=7 $1
+
 ./graph_stats.py dataset=postests yscale=linear $1
 ./graph_stats.py dataset=postests yscale=linear graphtype=daily $1
-./graph_stats.py dataset=postests yscale=linear graphtype=daily n_day_av=3 $1
+./graph_stats.py dataset=postests yscale=linear graphtype=daily n_day_av=7 $1
+
 ./graph_stats.py dataset=active yscale=linear $1
 ./graph_stats.py dataset=proprec yscale=linear $1
 
